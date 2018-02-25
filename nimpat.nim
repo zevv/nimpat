db07e195cafcbd361148a63a3e424ffe74b52c79
Ico Doornekamp <ico@pruts.nl>
2018-02-25 21:41:43 +0100

    Minimal works

--- nimpat.nim
+++ nimpat.nim
@@ -1,6 +1,22 @@
 
 {.compile: "lstrlib.c".}
 
+const LUA_MAXCAPTURES = 32
+
+type
+
+  Capture = 
+    init: cstring
+    len: int
+
+  MatchState =
+    matchdepth: int
+    src_init: cstring
+    src_end: cstring
+    p_end: cstring
+    level: int
+    capture: array[LUA_MAXCAPTURES, Capture];
+
 
 proc str_find_aux (find: int, s: cstring, p: cstring, init: int): int {. importc: "str_find_aux" .}
 
